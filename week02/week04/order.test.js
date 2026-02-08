1c34d7ed7f371dd605af0d043c6335bac75efafc
Yaseen <yaseenaskar50@gmail.com>
2026-02-08 17:34:21 -0500

    added provided test case and two others

--- week02/week04/order.test.js
+++ week02/week04/order.test.js
@@ -4,7 +4,33 @@ test("applies discount when total is greater than 100", () => {
   // Arrange
   const price = 25;
   const quantity = 5;
-  const expected = (115).toFixed(2);
+  const expected = (105).toFixed(2);
+
+  // Act
+  const result = processOrder(price, quantity);
+
+  // Assert
+  expect(result).toBe(expected);
+});
+
+test("does not apply discount when total is less than 100", () => {
+  // Arrange
+  const price = 10;
+  const quantity = 5;
+  const expected = (50).toFixed(2);
+
+  // Act
+  const result = processOrder(price, quantity);
+
+  // Assert
+  expect(result).toBe(expected);
+});
+
+test("edge case when total equal exactly 100", () => {
+  // Arrange
+  const price = 25;
+  const quantity = 4;
+  const expected = (100).toFixed(2);
 
   // Act
   const result = processOrder(price, quantity);
