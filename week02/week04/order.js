de76e8aeb6f54e8fded817127e56402d6ba45c1f
Yaseen <yaseenaskar50@gmail.com>
2026-02-08 15:47:32 -0500

    initial order.js error identification and fix

--- week02/week04/order.js
+++ week02/week04/order.js
@@ -2,9 +2,12 @@ function calculateTotal(price, quantity) {
   return price * quantity;
 }
 
+discount = 20;
+
 function applyDiscount(total) {
   if (total > 100) {
     return total - discount;
+// This is a ReferenceError in line 7, and the root cause is that "discount" is not defined and has no reference in the code.
   }
   return total;
 }
